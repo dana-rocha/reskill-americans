abea0ee9b0198642007590e38c6e2f9861c6f3c8
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2021-04-13 21:48:49 -0700

    Added feature to update user's password

--- atm-project/mock-atm.py
+++ atm-project/mock-atm.py
@@ -4,29 +4,47 @@
 # Last updated 3/31/21
 
 from datetime import datetime
+import random
 
-database = {'Seyi': "passwordSeyi", "Mike": "passwordMike", "Dana":"passwordDana"}
+database = {0000000000:["Dana", "passwordDana"]}
+
+def init():
+    print("Would you like to Login or Register?")
+    userIN = int(input("(1) Login (2) Register \n"))
+
+    if userIN == 1:
+        login()
+    elif userIN == 2:
+        register()
+    else:
+        # If no valid options selected, show options again
+        print("Invalid option selected.")
+        init()
 
 def login():
     print("**** Login ****")
     print("Welcome to the Bank!")
-    name = input("What is your name? \n")
+
+    accountNumberUser = int(input("What is your Account Number? \n"))
     password = input("What is your password? \n")
 
-    if (name in database and password == database[name]):
-        if (password == database[name]):
-            print("Welcome " + name)
-            currentDateTime = datetime.now()
-            formattedDateTime = currentDateTime.strftime("%m/%d/%y %H:%M:%S")
-            print("Last login at %s \n" % formattedDateTime)
-            
-            bankOperations()
-    else:
-        print("Username or Password is incorrect.")
-        print("Try again or Create an Account.")
-        init()
+    # Check user info in the database
+    for accountNum, userDetails in database.items():
+        if (accountNum == accountNumberUser):
+            if (userDetails[1] == password):
+                print(" =============================")
+                print("Welcome " + userDetails[0])
+                currentDateTime = datetime.now()
+                formattedDateTime = currentDateTime.strftime("%m/%d/%y %H:%M:%S")
+                print("Last login at %s \n" % formattedDateTime)
+                
+                bankOperations(userDetails)
+            else:
+                print("Login Failed. Username or Password is incorrect.")
+                print("Try again or Create an Account.")
+                init()
 
-def bankOperations():
+def bankOperations(user):
     option = ""
     currentBalance = 0
 
@@ -35,10 +53,11 @@ def bankOperations():
         print("1. Withdrawal")
         print("2. Cash Deposit")
         print("3. Complaint")
+        print("4. Update Your Password")
         print("Enter x to Log Out.")
-        print(" ======= =============== =======")
+        print(" =============================")
         selectedOption = input("Please select an option \n")
-        print(" ======= =============== =======")
+        print(" =============================")
         
         if (selectedOption == "1"):
             withdrawInput = int(input("How much would you like to withdraw? \n"))
@@ -52,34 +71,55 @@ def bankOperations():
         elif(selectedOption == "3"):
             complaintInput = input("What issue would you like to report? \n")
             print("Thank you for contacting us. Your complaint will be reported. \n")
+        elif(selectedOption == "4"):
+            updatePassword()
         elif(selectedOption == "x"):
             option = "x"
             print("Logging out of ATM.")
         else:
             print("Invalid option selected, please try again.")
 
-def init():
-    print("Would you like to Login or Register?")
-    userIN = int(input("(1) Login (2) Register \n"))
-
-    if userIN == 1:
-        login()
-    elif userIN == 2:
-        register()
-    else:
-        print("Invalid option selected.")
-        init()
-
 def register():
+    # Create new user
     print("**** Registration ****")
     new_first_name = input("What is your first name? \n")
     new_password = input("Create a password \n")
 
-    database[new_first_name] = new_password
+    accountNumber = generateAccountNumber()
+
+    # Add new user to the database
+    database[accountNumber] = [new_first_name, new_password]
 
     # Login to account after account has been created
+    print(" =============================")
     print("Your Account has been created.")
-    print(" ======= =============== =======")
+    print(" =============================")
+    print("Your Account Number is %d" % accountNumber)
+    print("Make sure you keep it safe.")
+    print(" =============================")
     login()
+
+def generateAccountNumber():
+    # Generate a random 10-digit number for the Bank Account
+    return random.randrange(1111111111, 9999999999)
+
+def updatePassword():
+    # Update a user's password
+    print("**** Update Your Password ****")
+    accountIN = int(input("What is your Account Number? \n"))
+    usernameIN = input("What is your name? \n")
+    passwordOLD = input("What is your old password? \n")
+    passwordNEW = input("What is your new password? \n")
+
+    # Double check user info before updating user's password
+    for acctNum, userInfo in database.items():
+        if (acctNum == accountIN and userInfo[0] == usernameIN):
+            if (userInfo[1] == passwordOLD):
+                print(" =============================")
+                # Update the second element in the dictionary key
+                userInfo[1] = passwordNEW
+                database[acctNum] = [usernameIN, userInfo[1]]
+                # Confirm new password to user
+                print("Hi, %s! Your new password is: %s" % (usernameIN, passwordNEW))
 
 init()

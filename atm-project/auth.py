46ce461f79ad9bb5cf71894fe13d287c63e90666
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2021-04-13 20:33:39 -0700

    Added functions from Seyi's example

--- atm-project/auth.py
+++ atm-project/auth.py
@@ -3,30 +3,92 @@
 # - Generate user account
 
 # Login
-# - Username, Password, email
+# - Account Number, Password
 
 # Bank Operations 
-
 # Initializing the system
-def init():
-    print("Welcome to Bank of Python")
-    haveAccount = int(input("Do you have an account with us: 1 = Yes, 2 = No"))
+import random
 
-    if (haveAccount == 1):
+database = {}
+
+def init():
+
+    isValidOption = False
+    print("Welcome to Bank of Python! \n")
+
+    haveAccount = int(input("Do you have an account with us: 1 = Yes, 2 = No \n"))
+    if(haveAccount == 1):
         login()
     elif(haveAccount == 2):
         register()
     else:
         print("You have selected an invalid option.")
+        init()
 
 def login():
-    print("This is the login function")
+    print("**** Login ****")
+
+    accountNumberUser = int(input("What is your Account Number? \n"))
+    password = input("What is your password? \n")
+
+    for accountNumber, userDetails in database.items():
+        if (accountNumber == accountNumberUser):
+            # Check if account number is correct
+            if (userDetails[3] == password):
+                # Check if password is correct
+
+                # If everything is correct, go to bank operations
+                bankOperation(userDetails)
+
+    # If something is incorrect, break out of the loop and go back to Login
+    print("Invalid Account Number or Password")
+    login()
 
 def register():
-    print("This is the register function")
+    print("**** Registration ****")
+    email = input("What is your email address? \n")
+    first_name = input("What is your first name? \n")
+    last_name = input("What is your last name? \n")
+    password = input("Create a password \n")
 
-def bankOperation():
-    print("some operations")
+    accountNumber = generateAccountNumber()
+
+    database[accountNumber] = [first_name, last_name, email, password]
+
+    # Login to account after account has been created
+    print("Your Account has been created.")
+    print(" ======= =============== =======")
+    print("Your Account Number is %d" % accountNumber)
+    print("Make sure you keep it safe.")
+    print(" ======= =============== =======")
+
+    login()
+
+def bankOperation(user):
+    print("Welcome %s %s" % (user[0], user[1]))
+
+    selectedOption = int(input("What would you like to do? \n(1) Deposit (2) Withdrawl (3) Logout (4) Exit \n"))
+
+    if (selectedOption == 1):
+        depositOperation()
+    elif(selectedOption == 2):
+        withdrawalOperation()
+    elif(selectedOption == 3):
+        login()
+    elif(selectedOption == 4):
+        exit()
+    else:
+        print("Invalid option selected.")
+        bankOperation(user)
+
+def withdrawalOperation():
+    print("withdrawal")
+
+def depositOperation():
+    print("deposit")
 
 def generateAccountNumber():
-    print("account number generator")
+    return random.randrange(1111111111, 9999999999)
+
+#### ACTUAL BANKING SYSTEM ####
+init()

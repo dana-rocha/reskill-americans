27f280dfb523b3342e503177837290f648606122
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2021-04-20 19:29:31 -0700

    Project skeleton

---
+++ atm-project/database.py
@@ -0,0 +1,36 @@
+# Create record
+# Update record
+# Read record
+# Delete record
+# CRUD
+
+def create(user_details):
+    print("Create a New Record")
+    # Create a file
+    # Name the file with the account_number.txt in the "data" folder
+    # Add the user details to the file
+    # Return True 
+
+def read(user_account_number):
+    print("Read User Record")
+    # Find user with account number
+    # Fetch content of the file
+
+
+def update(user_account_number):
+    print("Update User Record")
+    # Find the user with account number
+    # Fetch the contents of the file
+    # Update the contents of the file
+    # Save the file
+    # Return True
+
+def delete(user_account_number):
+    print("Delete User Record")
+    # Find the user with account number
+    # Delete the user record (file)
+    # Return True
+
+def find(user_account_number):
+    print("Find User")
+    # Find the user with account number

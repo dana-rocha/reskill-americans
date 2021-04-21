37b030a5fa0ed5632c07cb74e1dcbe5d859c918e
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2021-04-20 20:09:02 -0700

    Snakecase and create function

--- atm-project/auth.py
+++ atm-project/auth.py
@@ -9,20 +9,21 @@
 # Initializing the system
 import random
 import validation
+import database
 
-database = {
-    5154511908: ["Dana", "Rocha", "rocha.da@northeastern.edu", "password", 200]
-}
+#database = {
+#    5154511908: ["Dana", "Rocha", "rocha.da@northeastern.edu", "password", 200]
+#}
 
 def init():
 
-    isValidOption = False
+    is_valid_option = False
     print("Welcome to Bank of Python! \n")
 
-    haveAccount = int(input("Do you have an account with us: 1 = Yes, 2 = No \n"))
-    if(haveAccount == 1):
+    have_account = int(input("Do you have an account with us: 1 = Yes, 2 = No \n"))
+    if(have_account == 1):
         login()
-    elif(haveAccount == 2):
+    elif(have_account == 2):
         register()
     else:
         print("You have selected an invalid option.")
@@ -31,26 +32,27 @@ def init():
 def login():
     print("**** Login ****")
 
-    accountNumberUser = input("What is your Account Number? \n")
+    account_number_user = input("What is your Account Number? \n")
 
-    is_valid_account_number = validation.account_number_validation(accountNumberUser)
+    is_valid_account_number = validation.account_number_validation(account_number_user)
 
     if is_valid_account_number:
 
         password = input("What is your password? \n")
 
-        for accountNumber, userDetails in database.items():
-            if (accountNumber == int(accountNumberUser)):
-                # Check if account number is correct
-                if (userDetails[3] == password):
-                    # Check if password is correct
-                    # If everything is correct, go to bank operations
-                    bankOperation(accountNumber, userDetails)
-                else:
-                    # If something is incorrect, break out of the loop and go back to Login
-                    print("Invalid Account Number or Password")
-                    login()
+        # for account_number, user_details in database.items():
+        #     if (account_number == int(account_number_user)):
+        #         # Check if account number is correct
+        #         if (user_details[3] == password):
+        #             # Check if password is correct
+        #             # If everything is correct, go to bank operations
+        #          bank_occurrences(account_number, user_details)
+        #         else:
+        #             # If something is incorrect, break out of the loop and go back to Login
+        #             print("Invalid Account Number or Password")
+        #             login()
     else:
+        print("Invalid Account Number or Password")
         init()
 
 def register():
@@ -60,42 +62,49 @@ def register():
     last_name = input("What is your last name? \n")
     password = input("Create a password. \n")
     
-    accountNumber = generateAccountNumber()
+    account_number = generate_account_number()
     
-    database[accountNumber] = [first_name, last_name, email, password]
+    #database[account_number] = [first_name, last_name, email, password, balance]
+    # Use the database module to create new user record
+    # Create a file in the database
+    is_user_created = database.create(account_number, [first_name, last_name, email, password, 0])
 
     # Login to account after account has been created
-    print("Your Account has been created.")
-    print(" ======= =============== =======")
-    print("Your Account Number is %d" % accountNumber)
-    print("Make sure you keep it safe.")
-    print(" ======= =============== =======")
+    if is_user_created:
+        print("Your Account has been created.")
+        print(" ======= =============== =======")
+        print("Your Account Number is %d" % account_number)
+        print("Make sure you keep it safe.")
+        print(" ======= =============== =======")
 
-    login()
+        login()
+    else:
+        print("Something went wrong, please try again.")
+        register()
 
-def bankOperation(accNum, user):
+def bank_occurrences(acc_num, user):
     print("Welcome %s %s" % (user[0], user[1]))
 
     try:
-        selectedOption = int(input("What would you like to do? \n(1) Deposit (2) Withdrawl (3) Logout (4) Exit \n"))
+        selected_option = int(input("What would you like to do? \n(1) Deposit (2) Withdrawl (3) Logout (4) Exit \n"))
     
-        if (selectedOption == 1):
-            depositOperation()
-        elif(selectedOption == 2):
-            withdrawalOperation(accNum, user)
-        elif(selectedOption == 3):
+        if (selected_option == 1):
+            deposit_operation()
+        elif(selected_option == 2):
+            withdrawal_operation(acc_num, user)
+        elif(selected_option == 3):
             logout()
-        elif(selectedOption == 4):
+        elif(selected_option == 4):
             exit()
         else:
             print("Invalid option selected.")
-            bankOperation(accNum, user)
+            bank_occurrences(acc_num, user)
     
     except ValueError:
         print("You must enter a valid option. ")
-        bankOperation(accNum, user)
+        bank_occurrences(acc_num, user)
 
-def withdrawalOperation(acctNumber, user_details):
+def withdrawal_operation(acct_number, user_details):
     print("**** Withdrawal ****")
 
     # Get current balance
@@ -117,13 +126,13 @@ def withdrawalOperation(acctNumber, user_details):
             print("Current Balance: %d" % user_details[4])
         else:
             print("Insufficient funds for withdrawal transaction.")
-            bankOperation(acctNumber, user_details)
+            bank_occurrences(acct_number, user_details)
 
     except ValueError:
         print("You must enter a valid number.")
-        withdrawalOperation(user_details)
+        withdrawal_operation(user_details)
 
-def depositOperation():
+def deposit_operation():
     print("**** Deposit ****")
 
     # Get current balance
@@ -131,7 +140,7 @@ def depositOperation():
     # Add deposit amount to current balance
     # Display current balance
 
-def generateAccountNumber():
+def generate_account_number():
     return random.randrange(1111111111, 9999999999)
 
 def get_current_balance(user_details):
--- atm-project/database.py
+++ atm-project/database.py
@@ -4,12 +4,25 @@
 # Delete record
 # CRUD
 
-def create(user_details):
+def create(account_number, user_details):
     print("Create a New Record")
     # Create a file
     # Name the file with the account_number.txt in the "data" folder
     # Add the user details to the file
     # Return True 
+    completion_state = False
+
+    try:
+        f = open("data/user_record/" + str(account_number) + ".txt", "x")
+    except FileExistsError:
+        # Delete the already created file, print out error, then return False
+        print("User already exists.")
+    else:
+        f.write(str(user_details))
+        completion_state = True
+    finally:
+        f.close()
+    return completion_state
 
 def read(user_account_number):
     print("Read User Record")
@@ -34,3 +47,5 @@ def delete(user_account_number):
 def find(user_account_number):
     print("Find User")
     # Find the user with account number
+
+#create(5154511908, ["Dana", "Rocha", "rocha.da@northeastern.edu", "password", 200])

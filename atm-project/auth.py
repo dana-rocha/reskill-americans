5c8a712c5e729d72e4d857d3c4f3a27fd2ce468e
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2021-04-21 19:10:49 -0700

    Added comments

--- atm-project/auth.py
+++ atm-project/auth.py
@@ -19,9 +19,9 @@ def init():
 
     have_account = int(input("Do you have an account with us: 1 = Yes, 2 = No \n"))
     
-    if (have_account == 1):
+    if have_account == 1:
         login()
-    elif (have_account == 2):
+    elif have_account == 2:
         register()
     else:
         print("You have selected an invalid option.")
@@ -31,38 +31,45 @@ def login():
     print("**** Login ****")
 
     account_number_user = input("What is your Account Number? \n")
-
+    
+    # Validate the account number from user input
     is_valid_account_number = validation.account_number_validation(account_number_user)
 
+    # If account number from user input passes, get password
     if is_valid_account_number:
 
         # Hiding the user's password when typing it in the commandline
         password = getpass("What is your password? \n")
-
+        # Check if user's account number and password exist in the database
         user = database.authenticate_user(account_number_user, password)
 
         if user:
+            # If the account number and password exist in the database, carry out bank operations
             bank_operations(user)
 
         print("Invalid Account Number or Password")
         login()
 
     else:
+        # Account number from user input didn't pass validation
         print("Account Number Invalid: Check that you have up to 10 digits.")
         init()
 
 def register():
     print("**** Registration ****")
-
+    # Get new user's information
     email = input("What is your email address? \n")
     first_name = input("What is your first name? \n")
     last_name = input("What is your last name? \n")
+    
+    # Hiding the user's password when typing it in the commandline
     password = getpass("Create a password. \n")
     
+    # Generate a random 10-digit number for bank account
     account_number = generate_account_number()
     
     # Use the database module to create new user record
-    # Create a file in the database
+    # Create a new file in the database
     is_user_created = database.create(account_number, first_name, last_name, email, password)
 
     # Login to account after account has been created
@@ -89,20 +96,20 @@ def bank_operations(user):
         if (selected_option == 1):
             deposit_operation()
         elif(selected_option == 2):
-            withdrawal_operation(acc_num, user)
+            withdrawal_operation(user)
         elif(selected_option == 3):
             logout()
         elif(selected_option == 4):
             exit()
         else:
             print("Invalid option selected.")
-            bank_operations(acc_num, user)
+            bank_operations(user)
     
     except ValueError:
         print("You must enter a valid option. ")
         bank_operations(user)
 
-def withdrawal_operation(acct_number, user_details):
+def withdrawal_operation(user_details):
     print("**** Withdrawal ****")
 
     # Get current balance
@@ -119,12 +126,12 @@ def withdrawal_operation(acct_number, user_details):
             currentBalance -= withdrawalAmt
             user_details[4] = currentBalance
 
-            # Need to actually update the database dictionary with the new account balance
+            # Need to actually update the database with the new account balance
 
             print("Current Balance: %d" % user_details[4])
         else:
             print("Insufficient funds for withdrawal transaction.")
-            bank_operations(acct_number, user_details)
+            bank_operations(user_details)
 
     except ValueError:
         print("You must enter a valid number.")
--- atm-project/database.py
+++ atm-project/database.py
@@ -19,27 +19,32 @@ def create(user_account_number, first_name, last_name, email, password):
 
 
     if does_account_number_exist(user_account_number):
+        # Return False if account number exists already 
         return False
 
     if does_email_exist(email):
+        # If email is already being used, return Ralse 
         print("User already exists.")
         return False
 
+    # If code reaches to this point, the account number and email are not being used yet
     completion_state = False
-
+    
     try:
+        # Create a new text file with the account number
         f = open(user_db_path + str(User_account_number) + ".txt", "x")
 
     except FileExistsError:
-        # Delete the already created file, print out error, then return False
+        # If this filename already exists, delete the already created file, print out an error, then return False
         does_file_contain_data = read(user_db_path + str(user_account_number) + ".txt")
 
         # Check contents of file before deleting
-        # delete(account_number)
         if not does_file_contain_data:
+            # If the file doesn't contain anything, delete the file
             delete(user_account_number)
     
     else:
+        # If filename doesn't exist yet, create the file and write the user data to the file
         f.write(str(user_data))
         completion_state = True
     finally:
@@ -47,16 +52,19 @@ def create(user_account_number, first_name, last_name, email, password):
         return completion_state
 
 def read(user_account_number):
-    #print("Read User Record")
     # Find user with account number
     # Fetch content of the file
+
+    # Validating the user's account number
     is_valid_account_number = validation.account_number_validation(user_account_number)
     
     try:
 
         if is_valid_account_number:
+            # If the account number is validated, read the text file
             f = open(user_db_path + str(user_account_number) + ".txt", "r")
         else:
+            # If the account number isn't validated, read the file
             f = open(user_db_path + str(user_account_number), "r")
 
     except FileNotFoundError:
@@ -86,9 +94,11 @@ def delete(user_account_number):
     # Return True
     is_delete_successful = False
 
+    # Check if the path exists
     if os.path.exists(user_db_path + str(user_account_number) + ".txt"):
         
         try:
+            # If the file path exists, remove the file
             os.remove(user_db_path + str(user_account_number) + ".txt")
             is_delete_successful = True 
         except FileNotFoundError:
@@ -98,33 +108,39 @@ def delete(user_account_number):
 
 def does_email_exist(email):
     # Loop through every file in folder
+    # One file = one user
     all_users = os.listdir(user_db_path)
 
+    # Loop through all users
     for user in all_users:
+        # Create list of users
         user_list = str.split(read(user), ",")
 
+        # If the email is in the user list, then the email is already being used, return True
         if email in user_list:
             return True
-
+    # If email is not being used, return False 
     return False
 
 def does_account_number_exist(account_number):
     # Loop through every file in folder
     all_users = os.listdir(user_db_path)
 
+    # If user's account number already exists, return True
     for user in all_users:
         if (user == str(account_number) + ".txt"):
             return True
-
+    # If user's account number does not exist, return False
     return False
 
 def authenticate_user(account_number, password):
 
     if does_account_number_exist(account_number):
-
+        # If account number exists, create list of account numbers
         user = str.split(read(account_number), ",")
 
         if password == user[3]:
+            # If password is in user's file, return the user's account number
             return user
 
     return False

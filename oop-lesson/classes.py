cdaf44518969a3d6f75759775edf70934e941227
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2021-04-29 10:44:27 -0700

    Example on class inheritance

--- oop-lesson/classes.py
+++ oop-lesson/classes.py
@@ -38,10 +38,40 @@ myCar1 = Car("BMW", "White")
 myCar1.model = "Toyota"
 #myCar1.my_car()
 
+
 class MyClass:
     def __init__(self, height):
         self.height = 20
 
 myHeight = MyClass(100)
+# Can delete attributes
 del myHeight.height
-print(myHeight.height)
+#print(myHeight.height)
+
+
+# Derived class inherits features from the base class where new features can be added to it
+# Results in the reusuability of code
+class Person:
+
+    def __init__(self):
+        self.firstname = "Joshua"
+
+    def printname(self):
+        print(self.firstname)
+
+# User the Person class to create an object and execute the printname method:
+#X = Person("John", "Doe")
+#X.printname()
+
+
+# Consider the second class below which is the child class
+class Student(Person):
+    
+    # Can add the init function like in the Parent class
+    # When you add the init function to the child class, it will no longer
+    # inherit the parent's init funcion
+    def __init__(self, fname):
+        self.firstname = fname
+
+student1 = Student("Daniel")
+student1.printname()

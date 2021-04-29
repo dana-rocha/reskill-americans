c470d2067f9e92344694ef5e8a8218031c232da4
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2021-04-29 11:03:49 -0700

    Example on Super function

--- oop-lesson/classes.py
+++ oop-lesson/classes.py
@@ -51,27 +51,61 @@ del myHeight.height
 
 # Derived class inherits features from the base class where new features can be added to it
 # Results in the reusuability of code
+# class Person:
+
+#     def __init__(self):
+#         self.firstname = "Joshua"
+
+#     def printname(self):
+#         print(self.firstname)
+
+# # User the Person class to create an object and execute the printname method:
+# #X = Person("John", "Doe")
+# #X.printname()
+
+
+# # Consider the second class below which is the child class
+# class Student(Person):
+    
+#     # Can add the init function like in the Parent class
+#     # When you add the init function to the child class, it will no longer
+#     # inherit the parent's init funcion
+#     def __init__(self, fname):
+#         self.firstname = fname
+
+# student1 = Student("Daniel")
+# student1.printname()
+
+
+
+# Super function example
+# Makes the child class inherit all the properties and methods from the parent class
 class Person:
 
-    def __init__(self):
-        self.firstname = "Joshua"
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
 
     def printname(self):
-        print(self.firstname)
+        print(self.firstname, self.lastname)
 
-# User the Person class to create an object and execute the printname method:
-#X = Person("John", "Doe")
-#X.printname()
-
-
-# Consider the second class below which is the child class
 class Student(Person):
-    
-    # Can add the init function like in the Parent class
-    # When you add the init function to the child class, it will no longer
-    # inherit the parent's init funcion
-    def __init__(self, fname):
-        self.firstname = fname
 
-student1 = Student("Daniel")
-student1.printname()
+    def __init__(self, fname, lname, age, gender):
+        # Using the super function still lets 
+        # the child inherit the parent's attributes despite its own constructor
+        super().__init__(fname, lname)
+        # Can also add properties to the child class
+        self.age = age
+        self.gender = gender
+
+    # We can also define methods in the child class
+    def student_profile(self):
+        print("The student profile is listed below: \n")
+        print("First name: ", self.firstname)
+        print("Last name: ", self.lastname)
+        print("Age: ", self.age)
+        print("Gender: ", self.gender)
+
+x = Student("Mike", "Olsen", 24, "male")
+x.student_profile()
